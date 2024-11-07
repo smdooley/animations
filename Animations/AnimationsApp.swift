1574c2889b1259269a36d197c25693dce18ba2ed
Sean Dooley <s.dooley@ctidigital.com>
2024-11-07 15:08:17 +0000

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Sean Dooley on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
